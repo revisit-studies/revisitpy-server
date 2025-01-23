ab7673a4939994ff9152c78001ac8c64e35086d3
Brian Bollen <briancbollen@gmail.com>
2025-01-23 13:38:09 -0700

    Version 1.0

--- src/revisitpy_server/run_server.py
+++ src/revisitpy_server/run_server.py
@@ -1,14 +1,18 @@
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 import os
+from urllib.parse import urlparse
 
 
 class ReactHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
         # Serve index.html for all unknown paths
         static_dir = os.path.join(os.path.dirname(__file__), "static")
-        requested_path = os.path.join(static_dir, self.path.lstrip("/"))
+        parsed_path = urlparse(self.path)
+        clean_path = parsed_path.path.lstrip("/")  # Remove leading slash
+        requested_path = os.path.join(static_dir, clean_path)
 
         # Check if the requested path is a file
+        print(requested_path)
         if not os.path.isfile(requested_path):
             self.path = "/index.html"
             print(self.path)
